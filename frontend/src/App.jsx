ff1cdb3925436f8888569040514981590c4d5a3a
Angie Martinez <angieceballos2017@gmail.com>
2025-11-20 20:23:37 -0500

    guardo mis cambio locales

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/login.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className='container-form-login'>
+        <header className='header-login'>
+          <h1>Inicio de Sesión</h1>
+        </header>
+        <section className='fields-login'>
+          <label htmlFor="email">Correo</label>
+          <input type="email" id="Email" placeholder='usuario@donimio.com'/>
+
+          <label htmlFor="password">Contraseña</label>
+          <input type="password" id="Password" />
+        </section>
+
+        <section className='forgot-password'>
+          <a>¿Olvidaste tu Contraseña?</a>
+        </section>
+
+        <button>Ingresar</button>
+      <a>¿No tienes una cuenta? Registrate aqui</a>
+      </form>
     </>
   )
 }
