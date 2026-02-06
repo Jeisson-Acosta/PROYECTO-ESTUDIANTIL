1a3fd9e8a347bf20b1052d236b78a9815f989dca
Jeisson-Acosta <juanacostamillos69@gmail.com>
2026-02-05 23:06:07 -0500

    Some files are deleted and and advance is made login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,15 @@
-import './styles/Login.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Login } from './components/auth/Login.jsx'
+
 function App() {
 
   return (
     <>
-      <form className='container-form-login'>
-        <header className='header-login'>
-          <h1>Inicio de sesión</h1>
-        </header>
-
-        <section className='fields-login'>
-          <label htmlFor="email">Correo</label>
-          <input type="email" id='email' placeholder='usuario@dominio.com'/>
-
-          <label htmlFor="password">Contraseña</label>
-          <input type="password" id='password' />
-        </section>
-
-        <section className='forgot-password'>
-          <a>¿Olvidaste tu contraseña?</a>
-        </section>
-
-        <button>Ingresar</button>
-        <a>¿No tienes una cuenta? Registrate aquí</a>
-      </form>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </Router>
     </>
   )
 }
