8b3a0b31184bd0f8b6d0cc7bfaa0ff73157f507a
TemasDiego <queen4ever4545@gmail.com>
2026-02-28 18:55:57 -0500

    actualizacion

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-export function authMiddleware(req, res, next) {
-    const token = req.cookies.access_token
-    if (!token) {
-        return res.status(401).json({ ok: false, message: 'No autorizado' })
-    }
-    req.session = { user: null }
-=======
 import jwt from 'jsonwebtoken'
 import { SECRET_JWT_KEY } from '../config/config.js'
 
@@ -18,16 +10,11 @@ export function authMiddleware(req, res, next) {
 
     req.session = { user: null }
 
->>>>>>> 85f629f9513667d2430cc7c0e65b5efe38e2c5df
     try {
         const data = jwt.verify(token, SECRET_JWT_KEY)
         req.session.user = data
         next()
-<<<<<<< HEAD
-    } catch (error) {
-=======
     } catch {
->>>>>>> 85f629f9513667d2430cc7c0e65b5efe38e2c5df
         return res.status(401).json({ ok: false, message: 'Token invalido' })
     }
 }
