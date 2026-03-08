69841dcec18f5e509602efb5c99579f9bf87ac22
TemasDiego <queen4ever4545@gmail.com>
2026-03-07 23:37:06 -0500

    feat:Se trabajo en el dashboard creando los archivos StudentDashboard,StudentCard,MateriasCard y NotasCard, ademas se les dio estilos , quedan pendientes las graficas y el calendario, ademas de arreglar el error que impide que se obtengan los datos del backend y se puedan mostrar en la pagina

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ function Unauthorized() {
 
 
 
+
 function App() {
 
   return (
--- frontend/src/pages/student/StudentDashboard.jsx
+++ frontend/src/pages/student/StudentDashboard.jsx
@@ -1,13 +1,17 @@
 // common/Stats.jsx
 import  { useContext } from 'react'
-import '../../styles/common/Dashboard.css'
+import '../../styles/student/StudentDashboard.css'
 import { StudentCard } from '../../components/student/StudentCard.jsx'
 import { UserLoginContext } from '../../context/userLogin.jsx'
 import { MateriasCard } from '../../components/student/MateriasCard.jsx'
-import {IconStar,IconAsistance} from '../../components/common/IconsLayout.jsx'
+import {IconStar,IconAsistance,IconSubject} from '../../components/common/IconsLayout.jsx'
 import { NotasCard } from '../../components/student/NotasCard.jsx'
+import { CircleChart } from '../../components/common/charts/CircleChart.jsx'
 export function StudentDashboard() {
     const { userLogin } = useContext(UserLoginContext) || {};
+if (!userLogin) {
+    return null;
+}
     
 
    // const total_materias = userLogin.total_materias;
@@ -17,10 +21,11 @@ export function StudentDashboard() {
 
     return (
         <div className="principal-content">
+
             <div className='stats-dashboard'>
-                  <StudentCard titulo="Materias Inscritas" valor={userLogin?.total_materias || 2} adicional="+2 en el ciclo" />
-            <StudentCard titulo="Promedio Estudiante" valor={userLogin?.promedio || 4.8} adicional="top 5 en la clase" Icono={IconStar} />
-            <StudentCard titulo="Inasistencias" valor={userLogin?.total_inasistencias || 2} adicional="98% de asistencia" Icono= {IconAsistance} />
+                  <StudentCard titulo="Materias Inscritas" valor={userLogin?.total_materias || 2} adicional="+2 en el ciclo" Icono={IconSubject}/>
+            <StudentCard titulo="Promedio Estudiante" valor={userLogin?.promedio || 4.8} adicional="top 5 en la clase" Icono={IconStar}/>
+            <StudentCard titulo="Inasistencias" valor={userLogin?.total_inasistencias || 2} adicional="98% de asistencia" Icono= {IconAsistance}/>
             </div>
             <div className='stats-dashboard'>
             <div className="materias-seccion">
@@ -28,17 +33,18 @@ export function StudentDashboard() {
                    <h3 className='title'>Proximas Clases</h3>
                    <h3 className='dia'>HOY</h3>
             </div>
-         
+      
             <MateriasCard titulo="Ciencias" aula="Aula 1" hora="10:00"  profesor="Profesor 1" />
             <MateriasCard titulo="Matematicas" aula="Aula 2" hora="11:00"  profesor="Profesor 2" />
             </div>
         <div className='notas-seccion'>
         <div className='title-section'>
             <h3 className='title'>Ultimas Notas</h3>
-            <h3 className='title-more'>Ver mas</h3>
+            <h3 className='title-more'>Ver Todas</h3>
         </div>
         <NotasCard titulo="Ciencias" tarea ="Dibujar la mitocondria" calificacion="4.8"/>
         <NotasCard titulo="Matematicas" tarea ="Resolver Sumas" calificacion="4.0"/>
+        <NotasCard titulo="Español" tarea ="¿Que es una Oda" calificacion="3.7"/>
         </div>
         </div>
         </div>
