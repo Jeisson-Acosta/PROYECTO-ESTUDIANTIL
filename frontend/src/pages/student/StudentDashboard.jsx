e0e17e09e779ea4586011ec9cdf679a3295871b7
TemasDiego <queen4ever4545@gmail.com>
2026-03-08 20:59:13 -0500

    actualizacion de archivos

--- frontend/src/pages/student/StudentDashboard.jsx
+++ frontend/src/pages/student/StudentDashboard.jsx
@@ -21,14 +21,26 @@ export function StudentDashboard() {
 
     return (
         <div className="principal-content">
-            <CircleChart endValue={100} value={80} labelCenter="Promedio" color="#4400ffff"/>
 
             <div className='stats-dashboard'>
-                <StudentCard titulo="Materias Inscritas" valor={userLogin?.total_materias || 2} adicional="+2 en el ciclo" Icono={IconSubject}/>
-                <StudentCard titulo="Promedio Estudiante" valor={userLogin?.promedio || 4.8} adicional="top 5 en la clase" Icono={IconStar}/>
-                <StudentCard titulo="Inasistencias" valor={userLogin?.total_inasistencias || 2} adicional="98% de asistencia" Icono= {IconAsistance}/>
+                <StudentCard titulo="Materias Inscritas" valor={userLogin?.total_materias} adicional="+2 en el ciclo" Icono={IconSubject}/>
+                <StudentCard titulo="Promedio Estudiante" valor={userLogin?.promedio} adicional="top 5 en la clase" Icono={IconStar}/>
+                <StudentCard titulo="Inasistencias" valor={userLogin?.total_inasistencias} adicional="98% de asistencia" Icono= {IconAsistance}/>
             </div>
             <div className='stats-dashboard'>
+            
+
+            <div className='circle-chart-section'>
+            <div className='title-section'>
+                <h3 className='title'>Progreso Diario</h3>
+            </div>
+                <CircleChart endValue={100} value={80} labelCenter="Meta Hoy" color="#2c1470ff" strokeWidth={10}/>
+             <div className='circle-chart-resumen'>
+            <h3>Excelente Desempeño</h3>
+              
+            </div>
+            </div>
+           
             <div className="materias-seccion">
             <div className='title-section'>
                 <h3 className='title'>Proximas Clases</h3>
@@ -47,6 +59,14 @@ export function StudentDashboard() {
             <NotasCard titulo="Español" tarea ="¿Que es una Oda" calificacion="3.7"/>
         </div>
         </div>
+        <div className='stats-dashboard'>
+        <div className='title-section'>
+         
         </div>
+            
+        </div>
+            
+        </div>
+
     ) 
 }
