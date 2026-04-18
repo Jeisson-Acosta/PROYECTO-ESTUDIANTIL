5252f3011f85b9ce9b26519839fa3a5a1e221781
TemasDiego <queen4ever4545@gmail.com>
2026-04-18 18:18:14 -0500

    obtener de la rama JE

---
+++ backend/src/utils/getrecourse.js
@@ -0,0 +1,24 @@
+import path from 'node:path';
+import fs from 'node:fs';
+
+
+export function getRecourse(nombre_recurso) {
+    const uploaddir = path.resolve('uploads', 'resources');
+    let recourseBase64 = null;
+    try {
+        if (fs.existsSync(uploaddir)) {
+            const files = fs.readdirSync(uploaddir);
+            const recourseFile = files.find(f => f.startsWith(`${nombre_recurso}.`));
+
+            if (recourseFile) {
+                const filePath = path.join(uploaddir, recourseFile);
+                const fileBuffer = fs.readFileSync(filePath);
+                const ext = path.extname(recourseFile).substring(1); // remueve el punto
+                recourseBase64 = `data:image/${ext};base64,${fileBuffer.toString('base64')}`;
+            }
+        }
+        return recourseBase64;
+    } catch (error) {
+        console.error('Error al obtener el recurso:', error);
+    }
+}
